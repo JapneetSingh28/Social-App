9efb3ce921b6744d71b28e724e5dc70c3480886a
JapneetSingh28 <kingjapneetsingh@gmail.com>
2020-06-02 14:54:36 +0530

    Adding files

---
+++ android/app/src/main/kotlin/www/example/com/socialapphackintine/MainActivity.kt
@@ -0,0 +1,12 @@
+package www.example.com.socialapphackintine
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
